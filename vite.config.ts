c923407f2461b94422f19e19124a746c76472f74
SRT2801 <sahirruiz4@gmail.com>
2025-03-24 14:24:50 -0500

    [ADD] Agregar nuevas dependencias para mejorar la gestión de formularios y estilos

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Configurar el proxy para las peticiones a la API
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        // No es necesario reescribir la ruta porque ya incluye /api
+      },
+    },
+  },
+});
