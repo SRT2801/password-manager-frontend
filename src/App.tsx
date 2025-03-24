a5a0d60c425b6b49913b6477f900955827e5257e
SRT2801 <sahirruiz4@gmail.com>
2025-03-24 14:25:00 -0500

    [ADD] Implementar enrutamiento y agregar páginas de inicio de sesión y registro

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import MainLayout from './components/layout/MainLayout';
+import RegisterPage from './pages/register/RegisterPage';
+import LoginPage from './pages/login/LoginPage';
+import Card from './components/ui/Card';
+import { ToastProvider } from './context/ToastContext';
 
 export default function App() {
   return (
-    <MainLayout>
-      <div className="bg-white shadow-md rounded-lg p-6">
-        <h1 className="text-black font-semibold mb-8">Bienvenido a tu Gestor de Contraseñas</h1>
-        <p className="text-gray-600 mb-7">
-          Gestiona tus contraseñas de forma segura. Almacena, organiza y accede a tus credenciales
-          desde cualquier lugar.
-        </p>
-      </div>
-    </MainLayout>
+    <BrowserRouter>
+      <ToastProvider>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={
+            <MainLayout>
+              <Card>
+                <h1 className="text-black font-semibold mb-8">Bienvenido a tu Gestor de Contraseñas</h1>
+                <p className="text-gray-600 mb-7">
+                  Gestiona tus contraseñas de forma segura. Almacena, organiza y accede a tus credenciales
+                  desde cualquier lugar.
+                </p>
+              </Card>
+            </MainLayout>
+          } />
+        </Routes>
+      </ToastProvider>
+    </BrowserRouter>
   );
 }
