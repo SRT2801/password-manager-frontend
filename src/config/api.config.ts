a006d042e76317f795d29a6a123ea6b0ceacb035
SRT2801 <sahirruiz4@gmail.com>
2025-03-24 14:26:53 -0500

    [ADD] Agregar configuración de API con URL base y encabezados predeterminados

---
+++ src/config/api.config.ts
@@ -0,0 +1,8 @@
+export const API_CONFIG = {
+  // Con el proxy configurado, podemos usar una ruta relativa
+  BASE_URL: "/api",
+  HEADERS: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+};
